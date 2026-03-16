62eaa2fd9a64368205f58659ac82a39e8f451f21
Napattharaponghon <napattharapong.hon@spumail.net>
2026-03-16 12:27:50 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+particlesJS("magic-particles", {
+
+    particles: {
+
+        number: {
+            value: 80
+        },
+
+        color: {
+            value: "#d4af37"
+        },
+
+        shape: {
+            type: "circle"
+        },
+
+        opacity: {
+            value: 0.7
+        },
+
+        size: {
+            value: 3
+        },
+
+        move: {
+            enable: true,
+            speed: 1
+        }
+
+    },
+
+    interactivity: {
+
+        events: {
+
+            onhover: {
+                enable: true,
+                mode: "repulse"
+            }
+
+        }
+
+    }
+
+});
